69d5f69d6c8dd330b16e0c208c8444b1eee834d5
suppasentai <sonduc2210@gmail.com>
2018-06-22 13:03:48 +0700

    search

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,6 @@
 class StaticPageController < ApplicationController
   def home
     return unless Tour.any?
-    @feed_items = Tour.order_desc.paginate page: params[:page], per_page: Settings.record_pages
+    @feed_items = Tour.search_by_name(params[:search]).order_desc.paginate page: params[:page], per_page: Settings.record_pages
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,13 @@ User.create!(name: "Admin",
   password_confirmation: "admin123",
   role: 0)
 
-10.times do |n|
+20.times do |n|
   name = Faker::Name.name
   description = Faker::Lorem.paragraph(5)
   quantity = Faker::Number.between(10, 40)
   start_date = Faker::Date.forward(30)
   end_date = Faker::Date.between(start_date, start_date + Faker::Number.between(3, 10).days)
+  remote_picture_url = "https://www.vntrip.vn/cam-nang/wp-content/uploads/2017/06/Trang-an-ninh-binh-1.jpg"
   Tour.create!(name: name, description: description,quantity: quantity,
-    start_date: start_date, end_date: end_date, price: 2000)
+    start_date: start_date, end_date: end_date, price: 2000, remote_picture_url: remote_picture_url)
 end
