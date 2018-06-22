e680b50365e301d350c7ee38f500053df889cab5
TTPhuc <phucbitran@gmail.com>
2018-06-22 11:27:23 +0700

    Booking Tour

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def multiplication a, b
+    a * b
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,24 @@ User.create!(name: "Admin",
   password_confirmation: "admin123",
   role: 0)
 
+User.create!(name: "Example User",
+  email: "user@test.com",
+  phone: "0901111111",
+  bank_number: "111-222-2222",
+  address: "123 Suppasentai Street",
+  password: "user123",
+  password_confirmation: "user123",
+  role: 1)
+
 20.times do |n|
   name = Faker::Name.name
   description = Faker::Lorem.paragraph(5)
   quantity = Faker::Number.between(10, 40)
   start_date = Faker::Date.forward(30)
   end_date = Faker::Date.between(start_date, start_date + Faker::Number.between(3, 10).days)
+  seats_remaining = quantity
   remote_picture_url = "https://www.vntrip.vn/cam-nang/wp-content/uploads/2017/06/Trang-an-ninh-binh-1.jpg"
   Tour.create!(name: name, description: description,quantity: quantity,
-    start_date: start_date, end_date: end_date, price: 2000, remote_picture_url: remote_picture_url)
+    start_date: start_date, end_date: end_date, price: 2000, seats_remaining: seats_remaining,
+    remote_picture_url: remote_picture_url)
 end
